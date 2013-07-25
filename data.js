9ae6e02f42d23fcb4bd60cd5516b4f15b5d60984
Zach Keeshin <zkeesh@gmail.com>
2013-07-25 00:01:45 -0700

    This is the extent of the road trip stuff

---
+++ data.js
@@ -0,0 +1,166 @@
+var LOCATIONS = [
+	{
+		"lat": 33.8958492,
+		"lng": -118.2200712,
+		"city": "Compton, CA",
+		"name": "Compton High School" 
+	},
+	{
+		"lat": 33.9191799,
+		"lng": -118.4164652,
+		"city": "El Segundo, CA",
+		"name": "El Segundo High School"
+	},
+	{
+		"lat": 38.8338816,
+		"lng": -104.8213634,
+		"city": "Colorado Springs, CO",
+		"name": "Pikes Peak Prep"
+	},
+	{
+		"lat": 36.1539816,
+		"lng": -95.992775,
+		"city": "Tulsa, OK",
+		"name": "Union High School"
+	},
+	{
+		"lat": 34.9492613, 
+		"lng": -90.47149050000002,
+		"city": "Hughes, AR",
+		"name": "Hughes High School"
+	},
+	{
+		"lat": 32.8551326, 
+		"lng": -90.4056468,
+		"city": "Yazoo City,MS",
+		"name": "Woolfolk Middle School"
+	},
+	{
+		"lat": 29.9840922, 
+		"lng": -90.15285189999997, 
+		"city": "Metairie, LA",
+		"name": "Grace King High School"
+	},
+	{
+		"lat": 29.95106579999999, 
+		"lng": -90.0715323, 
+		"city": "New Orleans, LA",
+		"name": "Sci Academy"
+	},
+	{
+		"lat": 33.8839926, 
+		"lng": -84.51437609999999, 
+		"city": "Smyrna, GA",
+		"name": "Campbell High School"
+	},
+	{
+		"lat": 32.216316, 
+		"lng": -80.75260800000001, 
+		"city": "Hilton Head Island, SC",
+		"name": "Hilton Head Island High School"
+	},
+	{
+		"lat": 38.6265124, 
+		"lng": -76.91054830000002, 
+		"city": "Waldorf, MD",
+		"name": "Westlake High School"
+	},
+	//this school is in the same city
+		{
+		"lat": 38.6265124, 
+		"lng": -76.91054830000002, 
+		"city": "Waldorf, MD",
+		"name": "North Point High School"
+	},
+	{
+		"lat": 38.8851122, 
+		"lng": -76.91580679999998, 
+		"city": "Capitol Heights, MD",
+		"name": "Fairmont Heights High School"
+	},
+	{
+		"lat": 39.420384, 
+		"lng": -76.6699711,  
+		"city": "Brooklandville, MD",
+		"name": "St. Paul's School"
+	},
+	{
+		"lat": 39.952335, 
+		"lng": -75.16378900000001, 
+		"city": "Philadelphia,PA", 
+		"name": "Crossroads Academy"
+	},
+	{
+		"lat": 40.735657, 
+		"lng": -74.1723667 
+		"city": "Newark, NJ",
+		"name": "North Star Academy Charter School"
+	},
+	//this city has changed
+	{
+		"lat": 42.33176419999999, 
+		"lng": -71.12116349999997, 
+		"city": "Brookline, MA",
+		"name": "Beaver Country Day School"
+	},
+	{
+		"lat": 42.0917453, 
+		"lng": -70.7055871, 
+		"city": "Marshfield, MA",
+		"name": "Marshfield High School"
+	},
+	{
+		"lat": 41.74127730000001, 
+		"lng": -70.59893249999999, 
+		"city": "Bourne, MA",
+		"name": "Upper Cape Cod Regional Technical High School"
+	},
+	{
+		"lat": 43.563696, 
+		"lng": -70.20004670000003, 
+		"city": "Cape Elizabeth, ME",
+		"name": "Cape Elizabeth High School"
+	},
+	{
+		"lat": 43.7685392, 
+		"lng": -70.23285450000003,
+		"city": "Cumberland, ME",
+		"name": "Greely High School"
+	},	
+	{
+		"lat": 44.4192627, 
+		"lng": -72.01511829999998, 
+		"city": "St. Johnsbury, VT",
+		"name": "St. Johnsbury Academy"
+	},	
+	{
+		"lat": 41.8781136, 
+		"lng": -87.62979819999998, 
+		"city": "Chicago, IL",
+		"name": "Mather High School"
+	},	
+	{
+		"lat": 42.0039178, 
+		"lng": -87.97034610000003, 
+		"city": "Elk Grove Village, IL",
+		"name": "Elk Grove High School"
+	},	
+	{
+		"lat": 42.4436308, 
+		"lng": -88.23647749999998, 
+		"city": "Spring Grove, IL",
+		"name": "Homeschool"
+	},
+	{
+		"lat": 45.5234515, 
+		"lng": -122.6762071, 
+		"city": "Portland, OR",
+		"name": "Cathedral School"
+	},	
+	{
+		"lat": 38.5815719, 
+		"lng": -121.49439960000001, 
+		"city": "Sacremento, CA",
+		"name": "The Met Sacramento"
+	}
+];
